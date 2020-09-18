9e0b8585fb797da8e3b27bff545a2e0f53e6ab65
Mehtab <mehtab.ram@ajm.in>
2020-09-18 10:18:09 +0530

    Initial commit

---
+++ loghelper/src/main/java/com/loghelper/LogHelper.java
@@ -0,0 +1,37 @@
+package com.loghelper;
+
+import android.util.Log;
+
+
+public class LogHelper
+{
+	public static void systemOutPrint(String log_str)
+	{
+		if (BuildConfig.DEBUG)
+			System.out.println(log_str);
+	}
+
+	public static void systemErrPrint(String log_str)
+	{
+		if (BuildConfig.DEBUG)
+			System.err.println(log_str);
+	}
+
+	public static void printStackTrace(Exception e)
+	{
+		if (BuildConfig.DEBUG)
+			e.printStackTrace();
+	}
+
+	public static void logD(String tag, String log_str)
+	{
+		if (BuildConfig.DEBUG)
+			Log.d(tag, log_str);
+	}
+
+	public static void logI(String tag, String log_str)
+	{
+		if (BuildConfig.DEBUG)
+			Log.i(tag, log_str);
+	}
+}
